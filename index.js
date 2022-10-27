12736c6ead0560c5390b5394a1f161d0164e24c9
jeffbone95 <jeffbone95@gmail.com>
2022-10-27 17:36:30 -0600

    jswork

--- index.js
+++ index.js
@@ -22,7 +22,108 @@ const questions = [
             if(val==="") { 
                 return "jeffbone95"; 
                 
-            }
-        }
+            } 
+            return val; 
+        },
+    }, 
+   { 
+        type: "input",  
+        name: "image", 
+        message: "Input a supplemental image if disired", 
+        filter(val) { 
+            if(val=== "") { 
+                return "readme.PNG"; 
+            } 
+            return val; 
+        }, 
+        {  
+            type: "input", 
+            name: "email", 
+            message:"Please provide your email adress", 
+            filter(val) { 
+                if (val=== "") { 
+                    return "jeffbone95@gmail.com"; 
+                }
+            return val; 
+        }, 
+    },  
+    {  
+        type: "input", 
+        name: "installer", 
+        message: "What are the installation instructions?", 
+        filter(val) { 
+            if (val === "") { 
+                return '1. Download repo README \n 2. Run "node index.js"'; 
+            } 
+            return val;
+        }, 
+    }, 
+    { 
+        type: "input", 
+        name: "information", 
+        message: "info on this application", 
+        filter(val) { 
+            if (val === "") { 
+                return "after running node.js in the console, this app will create a README file";
+
+            } 
+            return val; 
+
+        },
+    }, 
+    { 
+        type: "input", 
+        name: "How-to", 
+        message: "Please follow prompts as given to the user", 
+        filter(val) { 
+            if (val=== "") { 
+                return "To proceed, type node.js in your console and follow instructions for operation";
+            } 
+            return val;
+        },
+    }, 
+    { 
+        type: "input", 
+        name: "deployment", 
+        message: "would you like to deploy?", 
+        filter(val) { 
+            if (val === "") { 
+                return false; 
+            } 
+            return true; 
+        },
+    }, 
+    function createMD(answers){ 
+        let strngToApnd = ""; 
+        let rtn = "\n\n\n"; 
+        strngToApnd += "#" + answers.ProjectTitle + rtn;
+   strngToApnd += "## Table of Contents \n"; 
+   strngToApnd += `1. [How-to](#How-to)\n`;  
+   strngToApnd += `2. [information](#information)\n`
+
+
+
+
+
     }
+
+
+
+
+    
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
 ]
